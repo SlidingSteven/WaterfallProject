2c93453249f7a213564cf14443e6e25610fd69a1
Steven Tucker <steven.tuckerba@ou.edu>
2019-08-30 12:42:02 -0500

    Deleted old project

--- waterfallProject/randomNum.py
+++
@@ -1,8 +0,0 @@
-import random
-f= open("rando.txt","w+")
-randoList = [] 
-for x in range(1000):
-    temp = str(random.randint(1,1000000))
-    f.write(temp + "\n")
-    randoList.append(temp)
-f.close()
