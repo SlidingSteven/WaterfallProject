71c3e796acc6b306eb8601eb875543bbe7070792
Steven Tucker <steven.tuckerba@ou.edu>
2019-08-30 12:40:27 -0500

    Pushing the Java side of the project

--- Waterfall2/mysite/polls/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-
-import random
-
-def getRando():
-    return random.randint(1,1000000)
-def index(request):
-    rando = getRando()
-    return HttpResponse(str(rando))
--- Waterfall2/mysite/randomNumber/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RandomnumberConfig(AppConfig):
-    name = 'randomNumber'
